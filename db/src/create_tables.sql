ffd46a4e9cffb71f32a8c2ca1876dc0414766bee
zenxha <komays5042@gmail.com>
2025-03-13 14:41:21 -0700

    fix primary key issue in db

--- db/src/create_tables.sql
+++ db/src/create_tables.sql
@@ -29,7 +29,7 @@ CREATE TABLE Store ( storeID integer NOT NULL,
                            PRIMARY KEY(storeID)
 );
 
-CREATE TABLE FoodOrder ( orderID SERIAL PRIMARY KEY,
+CREATE TABLE FoodOrder ( orderID SERIAL,
                            login varchar(50) NOT NULL, --places relationship
                            storeID integer NOT NULL, --placedAt relationship
                            totalPrice decimal(10,2) NOT NULL,
