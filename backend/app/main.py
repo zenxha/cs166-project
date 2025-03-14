cce5cc32c0ffed6757a778e8efef4aa628228f6d
zenxha <komays5042@gmail.com>
2025-03-13 17:07:51 -0700

    cors compliant

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import users, menu, order, store, auth
 from .database import engine, Base
 
+
 app = FastAPI()
+
+# Allowing CORS for the frontend URL
+origins = [
+    "http://localhost:5173", 
+    "http://localhost",  
+    "http://localhost:3000",  
+]
+
+# Add CORSMiddleware to the FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Database initialization
 Base.metadata.create_all(bind=engine)
